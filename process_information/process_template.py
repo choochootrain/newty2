9798560b8264e45d1658ffc155641df586f4e5a7
Lu Cheng <lu.cheng@berkeley.edu>
2012-05-26 23:23:59 -0700

    process information works now. just have to write get title get body and get date for general case

---
+++ process_information/process_template.py
@@ -0,0 +1,114 @@
+import urllib2
+from pymongo import Connection, ASCENDING, DESCENDING
+import re
+from Queue import Queue
+import os
+import sys
+import time
+import random
+import traceback
+from multiprocessing import Pool
+
+
+""" from pymongo import Connection
+c = Connection('localhost', 27018)
+db = c['wsj']
+db.drop_collection('explored')
+db.drop_collection('queue')"""
+
+
+
+
+
+
+begin_tags = r'<(div|td|tr|th|table|a|body|area|form|font|h1|h2|h3|h4|h5|h6|head|title|li|p|center|html|input|option|select|textarea|ul|i|b|label|footer|span)(>|[\s][^>^<^]*>)'
+end_tags = r'</(div|td|tr|th|table|a|body|area|form|font|h1|h2|h3|h4|h5|h6|head|title|li|p|center|html|input|option|select|textarea|ul|i|b|label|footer|span)(>|[\s][^>^<^]*>)'
+tags_regex = '<[^<^>]*>'
+
+"""Filters out the files that have already been explored"""
+def get_queue(main_dir, explored, queue):
+    files_list = get_files(main_dir)
+    for x in files_list:
+        if explored.find({'path' : x}).count() == 0:
+            entry = {'path' : x}
+            explored.insert(entry)
+            queue.insert(entry)
+    file_queue = []
+    print explored.count(), queue.count()
+    for x in queue.find():
+        file_queue.append(x)
+    return file_queue
+
+
+"""current dir begins with main_dir as current_dir and is recursive and finds all file paths in the main_dir"""
+def get_files(current_dir):
+    files_list = []
+    files = os.listdir(current_dir)
+    for file in files:
+        file_path = current_dir + '/' + file
+        if os.path.isdir(file_path):
+            files_list.extend(get_files(file_path))
+        else:
+            files_list.append(file_path)
+    return files_list
+
+
+
+
+
+
+def sort_information(queue_obj, get_title, get_body, get_date,
+                     testing, success, failure, queue):
+    file_path = queue_obj['path']
+    f = open(file_path, 'r')
+    html = f.read()
+    try:
+        title = get_title(html)
+        body = get_body(html)
+        date = get_date(html)
+        f.close()
+        entry = {'path' : file_path, 'title' : title, 'body' : body, 'date' : date}
+        if not testing:
+            success.insert(entry)
+    except:
+        if not testing:
+            failure.insert({'path' : file_path})
+        traceback.print_exc()
+    print file_path
+    if not testing:
+        queue.remove(queue_obj)
+
+def remove_tags(html):
+    tags = [(x.start(), x.end()) for x in re.finditer(tags_regex, html)]
+    clean_html = ''
+    prev_end = 0
+    for begin, end in tags:
+        clean_html += html[prev_end : begin]
+        prev_end = end
+    clean_html += html[prev_end : ]
+    return clean_html
+
+
+
+"""Map that lets you quite halfway or something in case something goes wrong"""
+def map_incremental(function, queue):
+    count = 0
+    while count + 100 < len(queue):
+        map(function, queue[count : count+100])
+        count += 100
+        print 'can quit here'
+        time.sleep(3)
+        print 'DO NOT QUIT'
+    map(function, queue[count :])
+
+
+def to_bytestring (self, s, enc='utf-8'):
+    """Convert the given unicode string to a bytestring, using the standard encoding,                                                                                                                     
+    unless it's already a bytestring"""
+    if s:
+        if isinstance(s, str):
+            return s
+        else:
+            return s.encode(enc)
+
+
---
+++ process_information/whatever_processor.py
@@ -0,0 +1,69 @@
+import sys
+from multiprocessing import Pool
+from process_template import *
+from pymongo import Connection, ASCENDING, DESCENDING
+
+"""Settings to edit"""
+"""If true, just prints stuff out and doesn't let you actually write to database"""
+testing = True
+database_name = 'insert database name here'
+
+
+connection = Connection('localhost', 27018)
+"""EDIT THE DATABASE NAME"""
+db = connection[database_name]
+success = db['success']
+failure = db['failure']
+queue = db['queue']
+explored = db['explored']
+"""success has path, title, date, body"""
+"""failure,queue, explored has path"""
+
+
+"""@param main_dir is the directory where all the files are"""
+main_dir = sys.argv[1]
+
+
+"""queue of files to be called upon for one function. If you have 
+multiple functions calling upon this create multiple queues"""
+file_queue = get_queue(main_dir, explored, queue)
+
+
+
+def main():
+    map_incremental(function_on_file, file_queue)
+    store_info = lambda queue_obj : sort_information(queue_obj, get_title, get_body,
+                                            get_date, testing, success, failure, queue)
+    map_incremental(store_info, file_queue)
+
+
+def function_on_file(queue_obj):
+    file_path = queue_obj['path']
+    f = open(file_path, 'r')
+    html = f.read()
+    f.close()
+    print file_path
+
+
+def get_title(html):
+    print 'get title not yet implemented'
+def get_body(html):
+    print 'get body not yet implemented'
+    a = html.find('<div class="body-copy">')
+    html1 = html[a + 23 :]
+    html_array = html1.split('</div>')
+    if len(html_array) < 2:
+        print 'failure'
+        return
+    body = html_array[1]
+    clean_body = remove_tags(body)
+    print clean_body
+def get_date(html):
+    print 'get date not yet implemented'
+    
+
+if __name__ == '__main__':
+    main()
+
+
+    
