be2ee4aeac814cc81e2bf2ffbdbc8de26ab569d3
Hurshal Patel <hpatel516@gmail.com>
2012-05-30 20:34:40 -0700

    actually restructured:

--- application1/admin.py
+++
@@ -1,4 +0,0 @@
-from application1.models import UserExtended
-from django.contrib import admin
-admin.site.register(UserExtended)
-
--- application2/admin.py
+++
@@ -1,4 +0,0 @@
-from application2.models import Model1, Model2
-from django.contrib import admin
-admin.site.register(Model1)
-admin.site.register(Model2)
