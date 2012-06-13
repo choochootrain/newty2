32ece6f3267f42f5325f6c41cb0a77bbb5b7047b
Lu Cheng <lu.cheng@berkeley.edu>
2012-06-12 21:58:23 -0700

    stuff is working

--- website/files/static/js/paths.js
+++ website/files/static/js/paths.js
@@ -1,5 +1,44 @@
+$(document).ajaxSend(function(event, xhr, settings) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?                                                                                        
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        function sameOrigin(url) {
+            // url could be relative or scheme relative or absolute                                                                                                
+            var host = document.location.host; // host + port                                                                                                      
+            var protocol = document.location.protocol;
+            var sr_origin = '//' + host;
+            var origin = protocol + sr_origin;
+            // Allow absolute or scheme relative URLs to same origin                                                                                               
+            return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+                (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+                // or any other URL that isn't scheme relative or absolute i.e relative.                                                                           
+                !(/^(\/\/|http:|https:).*/.test(url));
+        }
+
+        function safeMethod(method) {
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+
+        if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    });
+
+
 function get_path() {
-    return [[1300, 1], [1400, 1], [1184914800, 1], [1192518000, 1], [1197532800, 1], [1212994800, 1], [1214895600, 1], [1272351600, 1], [1291363200, 1]];
     x = {};
     x['word'] = getParameterByName('word');
     post_data = JSON.stringify(x);
