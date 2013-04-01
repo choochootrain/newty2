17ed9aacf3b77934df84affcc4f41362ec5e1be9
Lu Cheng <Lu@Lu-Chengs-MacBook-Pro.local>
2013-04-01 16:21:44 -0700

    add to nytimes date parser and add to nytimes body parser

--- aws/post_download/nytimes_handler.py
+++ aws/post_download/nytimes_handler.py
@@ -27,7 +27,10 @@ def get_body(html, url):
     if body1:
         return body1
     body2 = get_body2(html, url)
-    return body2
+    if body2:
+        return body2
+    print 'failure body', url
+    return False
 
 begin_matcher = re.compile('<p itemprop="articleBody">')
 def get_body1(html, url):
@@ -36,10 +39,8 @@ def get_body1(html, url):
     last_p = html.rfind('<p itemprop="articleBody">')
     end = html[last_p : ].find('</p>') + last_p
     body = html[first_p : end]
-    print first_p, end
     '''scripts in the body'''
     if body.find('<script>') != -1 or body.find('</script>') != -1:
-        print 'failure body', url
         return False
     clean_body = remove_tags(body).strip().replace('\n', ' ')
     clean_body = re.sub(r'\s+', ' ', clean_body)
@@ -47,32 +48,53 @@ def get_body1(html, url):
     return clean_body
 
 def get_body2(html, url):
-    return False
+    start_body = html.find('<div class="entry-content">') + len('<div class="entry-content">')
+    end_body = html.find('<div class="entry-meta">')
+    if start_body  == -1 or end_body  == -1:
+        return False
+    body =  html[start_body : end_body]
+    clean_body = remove_tags(body).strip().replace('\n', ' ')
+    clean_body = re.sub(r'\s+', ' ', clean_body)
+    clean_body = h.unescape(clean_body)
+    return clean_body
+
 
 def get_date(html, url):
     date1 = get_date1(html, url)
     if date1:
         return date1
     date2 = get_date2(html, url)
-    return date2
-
+    if date2:
+        return date2
+    date3 = get_date3(html, url)
+    if date3:
+        return date3
+    print 'failure date' , url
+    return False
 
 def get_date1(html, url):
+    date_begin = html.find('<meta name="ptime" content="') + len('<meta name="ptime" content="')
+    date_end = html[date_begin : ].find('"') + date_begin
+    return html[date_begin : date_end].strip()
+
+def get_date2(html, url):
     length = len('<meta itemprop="datePublished" content="')
     date_begin = html.find('<meta itemprop="datePublished" content="')
     date_end = html[date_begin:].find('">')
     if date_begin == -1 or date_end == -1:
-        print 'failure date', url
         return False
     date_end += date_begin
     date_begin += length
-
     date = html[date_begin : date_end]
     return date
 
-def get_date2(html, url):
-    return False
-
+def get_date3(html, url):
+    start_looking = html.find('<time datetime=')
+    date_begin = html[start_looking :].find('>') + len('>') + start_looking
+    date_end = html[date_begin:].find('</time>') + date_begin
+    if date_begin == -1 or date_end == -1:
+        return False
+    return html[date_begin : date_end]
 
 number_attached_letters = re.compile('(\d+)([A-Za-z]+)')
 def date_string_to_obj(date_string):
