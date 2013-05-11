a8a56cc6ffbe48875b9cc789b579cff0eb239aab
Hurshal Patel <hpatel516@gmail.com>
2013-05-10 22:57:32 -0700

    fix first change in stock

--- articles/techcrunch/scripts/extract_stock.py
+++ articles/techcrunch/scripts/extract_stock.py
@@ -1,10 +1,14 @@
 import csv
-file = "aapl_stock.csv"
+import os.path
+import sys
+
+basepath = os.path.dirname(__file__)
+file = os.path.abspath(os.path.join("..", 'data', "aapl_stock.csv"))
 reader = csv.reader(open(file),delimiter=',')
 reader.next()
 
 data = []
-last = 0
+last = -1
 mp = 0
 mmp = 10000000
 mc = 0
@@ -12,6 +16,10 @@ mmc = 10000000
 for row in reader:
   date = row[0] + 'T00:00:00';
   price = float(row[6])
+
+  if last == -1:
+    last = price
+
   change = price - last
   mp = max(mp, price)
   mc = max(mc, change)
