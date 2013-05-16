435fe0f8bb80e8bbadb54e6dda92b0553473a82c
Carl Shan <shan.carl@gmail.com>
2013-05-15 22:25:01 -0700

    Incomplete data.txt

--- sentiment/article_sentiment_analysis.py
+++ sentiment/article_sentiment_analysis.py
@@ -9,8 +9,11 @@ import os
 import re
 from collections import Counter
 from pandas import DataFrame
+import datetime as DT
+from matplotlib import pyplot as plt
+from matplotlib.dates import date2num
 
-f = open('techcrunch_articles.csv', 'rb')
+f = open('articles/techcrunch_articles.csv', 'rb')
 tc_data = DataFrame.from_csv(f, header=0)
 
 regex = re.compile('            Learn more  End of panel-container -->')
@@ -34,10 +37,47 @@ def extract_article_sentiment(cleaned_text):
     for word in bag:
         if word in word_sentiments.keys():
             sentiment += word_sentiments[word] * bag[word] # creates weighted sum of sentiment, based upon the nubmer of times that word appeared.
-    return sentiment / sum(bag.values())
+    if sum(bag.values()) > 0:
+        return sentiment / sum(bag.values()) # creates sentiment averaged over all unique words.
+    else:
+        return 0
+
+def normalize(sentiment):
+    ''' 
+    Normalizes sentiment value between -1 and 1 using min/max
+    Both the minimum and maximum sentiment were calculated beforehand.
+    '''
+    max_sent = 0.25 
+    min_sent = -0.0625
+    normalized_value = (sentiment - min_sent) / (max_sent - min_sent)
+    if abs(sentiment) == sentiment: # if the sentiment is positive, then keep the sign
+        return normalized_value
+    else:
+        return -1 * normalized_value
 
 if __name__ == '__main__':
     articles = tc_data['body']
-    d = dict()
-    for article in articles:
-        cleaned_article = clean_words(article)
+    data = []
+    for article, index, author in zip(articles, tc_data.index, tc_data['author']):
+        if article == article:
+            article_dict = {}
+            
+            # Getting the sentiment value
+            cleaned_article = clean_words(article)
+            sentiment = extract_article_sentiment(cleaned_article)
+            normalized_sentiment = normalize(sentiment)
+            article_dict['link'] = index
+            article_dict['sentiment'] = normalized_sentiment
+            article_dict['author'] = author
+            ## Getting the dates
+            link = index.split('/') #
+            article_dict['date'] = (link[3] + '-' + link[4] + '-' + link[5])# date2num(DT.datetime.strptime(link[3] + link[4] + link[5], '%Y%m%d'))
+            
+            data.append(article_dict)
+            print article_dict
+            ## Getting the dates
+            # x.append(date2num(DT.datetime.strptime(link[3] + link[4] + link[5], '%Y%m%d')))
+            # y.append(sentiment)
+    # plt.plot(x, y)
+        #print (index, sentiment)
+    #print data
