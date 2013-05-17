8bb85f8b824bf32079dd9aee0c1d8dfb1f6efda5
Carl Shan <shan.carl@gmail.com>
2013-05-16 18:22:34 -0700

    Readme

--- sentiment/article_sentiment_analysis.py
+++ sentiment/article_sentiment_analysis.py
@@ -2,16 +2,10 @@
 Sentiment analysis adapted from basic_sentiment_analysis.py and trends.py
 '''
 
-from pprint import pprint
-from data import word_sentiments # word_sentiments is a dictionary of words with a sentiment between -1 (negative) to 1 (positive)
-import nltk
-import os
+from data import word_sentiments  # word_sentiments is a dictionary of words with a sentiment between -1 (negative) to 1 (positive)
 import re
 from collections import Counter
 from pandas import DataFrame
-import datetime as DT
-from matplotlib import pyplot as plt
-from matplotlib.dates import date2num
 
 f = open('articles/techcrunch_articles.csv', 'rb')
 tc_data = DataFrame.from_csv(f, header=0)
@@ -32,15 +26,14 @@ def bag_of_words(words):
 def extract_article_sentiment(cleaned_text):
     # Step 1: Create Bag of Words
     bag = bag_of_words(cleaned_text)
+    if sum(bag.values()) == 0:  # checking against the edge case of if there are no words in the article.
+        return 0
     # Step 2: Update Sentiment
     sentiment = 0
     for word in bag:
         if word in word_sentiments.keys():
-            sentiment += word_sentiments[word] * bag[word] # creates weighted sum of sentiment, based upon the nubmer of times that word appeared.
-    if sum(bag.values()) > 0:
-        return sentiment / sum(bag.values()) # creates sentiment averaged over all unique words.
-    else:
-        return 0
+            sentiment += word_sentiments[word] * bag[word]  # creates weighted sum of sentiment, based upon the nubmer of times that word appeared.
+    return sentiment / sum(bag.values())  # creates sentiment averaged over all unique words.
 
 
 def normalize(sentiment):
@@ -52,10 +45,7 @@ def normalize(sentiment):
     min_sent = -0.0625
     normalized_value = sentiment / (abs(max_sent) + abs(min_sent) / 2)
     return normalized_value
-    #if abs(sentiment) == sentiment: # if the sentiment is positive, then keep the sign
-    #    return normalized_value
-    #else: # otherwise, since the normalization changes negatives to positives, we want to the negative sign back
-    #    return -1 * normalized_value
+
 
 if __name__ == '__main__':
     articles = tc_data['body']
@@ -63,7 +53,7 @@ if __name__ == '__main__':
     for article, index, author in zip(articles, tc_data.index, tc_data['author']):
         if article == article:
             article_dict = {}
- 
+
             # Getting the sentiment value
             cleaned_article = clean_words(article)
             sentiment = extract_article_sentiment(cleaned_article)
@@ -74,6 +64,6 @@ if __name__ == '__main__':
             ## Getting the dates
             link = index.split('/')
             article_dict['date'] = (link[3] + '-' + link[4] + '-' + link[5])
-            
+
             data.append(article_dict)
             print article_dict
